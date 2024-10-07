2f0c4fc067ca71bbd54204a6adf507105bc4bdcc
GitHub Action <action@github.com>
2024-10-07 08:21:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/ptst/intellijsalesforcecustomlabels/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/ptst/intellijsalesforcecustomlabels/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.ptst.intellijsalesforcecustomlabels.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
